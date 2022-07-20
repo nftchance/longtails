be0738608706aed1e6760c93b5df58ada053e4d6
nftchance <chance@utc24.io>
2022-07-20 14:04:03 -0500

    adding freemason determination

--- freemasons/models.py
+++ freemasons/models.py
@@ -1,27 +1,35 @@
 import datetime
 import django
+import requests
 
 from django.db import models
 
-from requests import request
-
 # Designed to support docs/social/freemason-frontrunning.md
 # Create FreeMasonProject
-#       - From that time on Longtails will watch the follower records of 
+#       - From that time on Longtails will watch the follower records of
 #         holders of that project
-# Every 12 hours a clock runs to refresh the primary brand 
+# Every 12 hours a clock runs to refresh the primary brand
 #       members (alpha generators)
 
 # 12 hours
 SECONDS_BETWEEN_SYNC = 60 * 60 * 12
 
+URLS = {
+    "MEMBERS": "http://www.nftinspect.xyz/api/collections/members/{0}?limit=2000&onlyNewMembers=false"
+}
+
 class TwitterUser(models.Model):
+    identifier = models.CharField(max_length=256, null=True)
+
+    name = models.CharField(max_length=256, null=True)
     username = models.CharField(max_length=256)
-    global_followings = models.PositiveIntegerField(default=0)
-    global_followers = models.PositiveIntegerField(default=0)
+
+    pfp_url = models.TextField(null=True)
+    token = models.CharField(max_length=256, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 class FreeMasonMember(models.Model):
     twitter = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
@@ -42,23 +50,44 @@ class FreeMasonProject(models.Model):
     contract_address = models.CharField(max_length=256)
     members = models.ManyToManyField(FreeMasonMember)
 
-    last_sync_at = models.PositiveIntegerField(default=0)
+    last_sync_at = models.DateTimeField(null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def needs_sync(self):
+        if not self.last_sync_at:
+            return True
+
         return self.last_sync_at - datetime.timedelta(seconds=SECONDS_BETWEEN_SYNC) > django.utils.timezone.now()
 
     def sync(self):
-        url = f"http://nftinspect.xyz/{self.contract_address}/members"
-        response = request.get(url)
+        response = requests.get(URLS["MEMBERS"].format(self.contract_address))
 
         if response.status_code == 200:
             response_data = response.json()
-            print(response_data)
+
+            self.members.clear()
+            for member in response_data['members']:
+                member_twitter_obj, created = TwitterUser.objects.get_or_create(
+                    identifier=member['id'],
+                )
+
+                member_twitter_obj.name = member['name']
+                member_twitter_obj.username = member['username']
+                member_twitter_obj.pfp_url = member['pfpUrl']
+                member_twitter_obj.token = member['token']
+                member_twitter_obj.save()
+
+                member_obj, created = FreeMasonMember.objects.get_or_create(
+                    twitter=member_twitter_obj,
+                )
+
+                self.members.add(member_obj)
+
             self.last_sync_at = django.utils.timezone.now()
             self.save()
-        else:
-            pass
+
+            return {"status": 200}
+        return {"status": 500}
--- freemasons/tests.py
+++ freemasons/tests.py
@@ -1,1 +1,14 @@
 from django.test import TestCase
+
+from freemasons.models import FreeMasonProject
+
+class FreeMasonsTestCase(TestCase):
+    def setUp(self):
+        self.project, self.created = FreeMasonProject.objects.get_or_create(
+            contract_address="0x23581767a106ae21c074b2276d25e5c3e136a68b"
+        )
+
+    def test_sync(self):
+        """ Can pull down collection members from data source"""
+        sync_response = self.project.sync()
+        self.assertEqual(sync_response['status'], 200)
