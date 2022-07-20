2bd6039f4605eb4ce274e25aadd21683ec1c8047
nftchance <chance@utc24.io>
2022-07-20 13:24:52 -0500

    connecting freemason to database

--- freemasons/models.py
+++ freemasons/models.py
@@ -1,4 +1,9 @@
+import datetime
+import django
+
 from django.db import models
+
+from requests import request
 
 # Designed to support docs/social/freemason-frontrunning.md
 # Create FreeMasonProject
@@ -6,6 +11,9 @@ from django.db import models
 #         holders of that project
 # Every 12 hours a clock runs to refresh the primary brand 
 #       members (alpha generators)
+
+# 12 hours
+SECONDS_BETWEEN_SYNC = 60 * 60 * 12
 
 class TwitterUser(models.Model):
     username = models.CharField(max_length=256)
@@ -19,13 +27,18 @@ class FreeMasonMember(models.Model):
     twitter = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
     wallet_address = models.CharField(max_length=256)
 
-    followers = models.ManyToManyField(TwitterUser)
-    following = models.ManyToManyField(TwitterUser)
+    followers = models.ManyToManyField(TwitterUser, related_name="followers")
+    following = models.ManyToManyField(TwitterUser, related_name="following")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class FreeMasonProject(models.Model):
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.needs_sync:
+            self.sync()
+
     contract_address = models.CharField(max_length=256)
     members = models.ManyToManyField(FreeMasonMember)
 
@@ -34,4 +47,18 @@ class FreeMasonProject(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @property
+    def needs_sync(self):
+        return self.last_sync_at - datetime.timedelta(seconds=SECONDS_BETWEEN_SYNC) > django.utils.timezone.now()
+
     def sync(self):
+        url = f"http://nftinspect.xyz/{self.contract_address}/members"
+        response = request.get(url)
+
+        if response.status_code == 200:
+            response_data = response.json()
+            print(response_data)
+            self.last_sync_at = django.utils.timezone.now()
+            self.save()
+        else:
+            pass
