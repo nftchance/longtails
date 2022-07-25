703668366db28e81b5c13aa5609226571cde1987
nftchance <chance@utc24.io>
2022-07-25 12:26:18 -0500

    removing static files

--- longtails/settings.py
+++ longtails/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 
     'freemasons',
     'twitter'
