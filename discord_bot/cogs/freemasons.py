1c59f6a42db5df811dc1d6ebec2d28425bfaf259
nftchance <chance@utc24.io>
2022-07-31 23:11:48 -0500

    implementing watched

--- discord_bot/cogs/freemasons.py
+++ discord_bot/cogs/freemasons.py
@@ -9,7 +9,7 @@ from discord.ext import commands, tasks
 
 from asgiref.sync import sync_to_async
 
-from freemasons.models import SECONDS_BETWEEN_SYNC, FreeMasonProject
+from freemasons.models import SECONDS_BETWEEN_SYNC, FreeMasonMember, FreeMasonProject, TwitterUser
 from twitter.client import TwitterClient
 
 
@@ -58,7 +58,8 @@ class FreeMasons(commands.Cog):
 
         for project_obj in projects.all():
             if not project_obj.next_sync_at or project_obj.next_sync_at < django.utils.timezone.now():
-                print(f'[FreeMasons] [Sync] [Project] {project_obj.contract_address}')
+                print(
+                    f'[FreeMasons] [Sync] [Project] {project_obj.contract_address}')
                 embed = discord.Embed(
                     title=f"[SYNCING] {project_obj.name}"
                 )
@@ -81,7 +82,8 @@ class FreeMasons(commands.Cog):
             for member in project_obj.members.filter(
                 next_sync_at__lte=django.utils.timezone.now()
             ) | project_obj.members.filter(next_sync_at__isnull=True):
-                print(f'[FreeMasons] [Sync] [Member] {member.twitter.username}')
+                print(
+                    f'[FreeMasons] [Sync] [Member] {member.twitter.username}')
                 await member.sync(self.twitter_client)
 
             if not project_obj.last_summarized_at or project_obj.last_summarized_at < django.utils.timezone.now() - datetime.timedelta(seconds=SECONDS_BETWEEN_SYNC):
@@ -119,16 +121,54 @@ class FreeMasons(commands.Cog):
 
         await self.longtails_channel.send(embed=embed)
 
-
     @app_commands.command(name="watching")
     async def watching(self, interaction: discord.Interaction) -> None:
         await interaction.response.send_message("Logging statuses.", ephemeral=True)
 
         projects = FreeMasonProject.objects.filter(watching=True)
- 
+
         embed = discord.Embed(
             title=f"[Watching] Summary",
-            description="\n".join(f"[{project_obj.name}]({project_obj.twitter})" for project_obj in projects.all())
+            description="\n".join(
+                f"[{project_obj.name}]({project_obj.twitter})" for project_obj in projects.all())
+        )
+
+        await self.longtails_channel.send(embed=embed)
+
+    @app_commands.command(name="watched")
+    async def watched(self, interaction: discord.Interaction, twitter_username: str) -> None:
+        await interaction.response.send_message("Grabbing the data that we have for that username.")
+
+        # get twitter object of username we are searching
+        twitter_user_obj = TwitterUser.objects.filter(
+            username=twitter_username)
+
+        # return no data message if we don't have this user in the database
+        if not twitter_user_obj.exists():
+            embed = discord.Embed(
+                title=f"[Watching] No Data Found For {twitter_username}",
+            )
+
+            await self.longtails_channel.send(embed=embed)
+
+            return
+
+        # find members that are following that user
+        members = FreeMasonMember.objects.filter(
+            following__in=[twitter_user_obj, ])
+
+        members = members.all().values('twitter_identifier', 'username').distinct()
+
+        description = "\n".join(
+            [f"[{member_inst.username}](https://twitter.com/i/user/{member_inst.twitter_identifier})" for member_inst in members.all()[:50]])
+
+        if members.count() > 50:
+            description += f"\nAnd {members.count() - 50} others."
+
+        # return output
+        embed = discord.Embed(
+            title=f"[Watching] Free Mason Members Following {twitter_username}",
+            description=description
         )
 
         await self.longtails_channel.send(embed=embed)
